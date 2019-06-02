340c0f665dca5bb43d279f6d6ac86ff37c67a229
171250625@smail.nju.edu.cn <171250625@smail.nju.edu.cn>
2019-06-02 20:29:50 +0800

    退票策略实现

--- src/main/java/com/example/cinema/blImpl/sales/RefundStrategyForBl.java
+++ src/main/java/com/example/cinema/blImpl/sales/RefundStrategyForBl.java
@@ -5,5 +5,15 @@ import com.example.cinema.vo.RefundTicketStrategyForm;
 import java.util.List;
 
 public interface RefundStrategyForBl {
+    /**
+     * 得到所有退票策略
+     * @return
+     */
     List<RefundTicketStrategyForm> getRefundStrategy();
+
+    /**
+     * 得到最优退票策略的折扣，如果不能退票，则返回0
+     * @return
+     */
+    double getBestRefundStrategy();
 }
--- src/main/java/com/example/cinema/blImpl/sales/RefundTicketStrategyServiceImpl.java
+++ src/main/java/com/example/cinema/blImpl/sales/RefundTicketStrategyServiceImpl.java
@@ -1,11 +1,38 @@
 package com.example.cinema.blImpl.sales;
 
 import com.example.cinema.bl.sales.RefundTicketStrategyService;
+import com.example.cinema.data.sales.RefundTicketMapper;
+import com.example.cinema.vo.RefundTicketStrategyForm;
+import com.example.cinema.vo.ResponseVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class RefundTicketStrategyServiceImpl implements RefundStrategyForBl, RefundTicketStrategyService {
     @Autowired
+    private RefundTicketMapper refundTicketMapper;
+
+    @Override
+    public ResponseVO publishRefundTicketStrategy(RefundTicketStrategyForm refundTicketStrategyForm){
+        return null;
+    }
+
+
+    @Override
+    public ResponseVO updateRefundTicketStrategy(RefundTicketStrategyForm refundTicketStrategyForm){
+        return null;
+    }
+
+    @Override
+    public List<RefundTicketStrategyForm> getRefundStrategy(){
+        return null;
+    }
+
+    @Override
+    public double getBestRefundStrategy(){
+        return 0;
+    }
 
 }
