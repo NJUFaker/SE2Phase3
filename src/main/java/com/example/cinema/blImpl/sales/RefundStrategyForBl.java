b6e0298937f8de0df2099de38936b2ba3b17ef91
forsakenspirit <2369351080@qq.com>
2019-06-02 20:56:21 +0800

    fixed sth

--- src/main/java/com/example/cinema/blImpl/sales/RefundStrategyForBl.java
+++ src/main/java/com/example/cinema/blImpl/sales/RefundStrategyForBl.java
@@ -2,6 +2,7 @@ package com.example.cinema.blImpl.sales;
 
 import com.example.cinema.vo.RefundTicketStrategyForm;
 
+import java.util.Date;
 import java.util.List;
 
 public interface RefundStrategyForBl {
@@ -15,5 +16,5 @@ public interface RefundStrategyForBl {
      * 得到最优退票策略的折扣，如果不能退票，则返回0
      * @return
      */
-    double getBestRefundStrategy();
+    double getBestRefundStrategy(Date date);
 }
--- src/main/java/com/example/cinema/data/promotion/VIPActivityMapper.java
+++ src/main/java/com/example/cinema/data/promotion/VIPActivityMapper.java
@@ -1,4 +1,20 @@
 package com.example.cinema.data.promotion;
 
+import com.example.cinema.po.VIPActivity;
+import com.example.cinema.vo.VIPActivityForm;
+
+import java.util.List;
+
 public interface VIPActivityMapper {
+
+    List<VIPActivity> getAllVIPActivities();
+
+
+    VIPActivity getVIPActivityById(int id);
+
+    int insertActivity(VIPActivityForm vipActivityForm);
+
+    void updateActivity(VIPActivityForm vipActivityForm);
+
+    void deleteActivity(int id);
 }
