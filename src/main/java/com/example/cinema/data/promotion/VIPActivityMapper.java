9c9c088c414479bb3415e23b2fcabadf53a4afec
171250625@smail.nju.edu.cn <171250625@smail.nju.edu.cn>
2019-06-02 23:05:11 +0800

    加了个mapper

--- src/main/java/com/example/cinema/data/promotion/VIPActivityMapper.java
+++ src/main/java/com/example/cinema/data/promotion/VIPActivityMapper.java
@@ -2,9 +2,10 @@ package com.example.cinema.data.promotion;
 
 import com.example.cinema.po.VIPActivity;
 import com.example.cinema.vo.VIPActivityForm;
+import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
-
+@Mapper
 public interface VIPActivityMapper {
 
     List<VIPActivity> getAllVIPActivities();
