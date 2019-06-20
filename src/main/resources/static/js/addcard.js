6de8c658eace580dccd2748c14cbf9de20753054
snowfungus <kingsnowmushroom@126.com>
2019-06-20 23:14:56 +0800

    不晓得有没有冲突

--- src/main/resources/static/js/addcard.js
+++ src/main/resources/static/js/addcard.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 $(document).ready(function () {
 
     getAllCard();
@@ -145,26 +144,3 @@ $(document).ready(function () {
 
 
 });
-=======
-$(document).ready(function() {
-    // getcardform();
-    //
-    // function getcardform() {
-    //     return {
-    //         movieId: movieId,
-    //         name: $('#movie-name-input').val(),
-    //         startDate: $('#movie-date-input').val(),
-    //         posterUrl: $('#movie-img-input').val(),
-    //         description: $('#movie-description-input').val(),
-    //         type: $('#movie-type-input').val(),
-    //         length: $('#movie-length-input').val(),
-    //         country: $('#movie-country-input').val(),
-    //         starring: $('#movie-star-input').val(),
-    //         director: $('#movie-director-input').val(),
-    //         screenWriter: $('#movie-writer-input').val(),
-    //         language: $('#movie-language-input').val()
-    //     };
-    // }
-
-})
->>>>>>> bb0ae0b4372af33ff54dcf2413bd89157b38fc33
