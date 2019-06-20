a2b78bbcc8f4e2e1cfc14983749f10da107018da
171250625@smail.nju.edu.cn <171250625@smail.nju.edu.cn>
2019-06-20 20:08:15 +0800

    添加30-34用例的单元测试

--- src/main/java/com/example/cinema/blImpl/sales/CouponServiceForBl.java
+++ src/main/java/com/example/cinema/blImpl/sales/CouponServiceForBl.java
@@ -18,5 +18,12 @@ public interface CouponServiceForBl {
      */
      Coupon getCouponById(Integer couponId);
      void deleteCoupon(int couponId,int userId);
+
+    /**
+     * 得到该用户在票的总价为total的情况下可以使用的优惠券
+     * @param total 票的总价
+     * @param userId
+     * @return
+     */
      List<Coupon> selectCouponByUserAndAmount(double total,int userId);
 }
